59daa3fd6f1368a3fbdaa0f1c856d91006869d82
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-08-04 11:22:50 +0700

    Them bt

---
+++ thlocaltime/src/main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Bây giờ là : " + now);
+    }
+}
